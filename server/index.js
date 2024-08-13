215fa347dbf110e050936c79ede70bf8085fedac
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-08-13 16:28:42 -0600

    chore: Add user account endpoints and product endpoints

--- server/index.js
+++ server/index.js
@@ -12,7 +12,46 @@ app.get("/", (req, res) => {
     res.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-app.get("/states", db.getStates);
+// ACCOUNTS
+app.post("/register", (req, res) => {
+    res.send("User registered successfully");
+});
+app.post("/login", (req, res) => {
+    res.send("User logged in successfully");
+});
+app.get("/profile/:email", (req, res) => {
+    const email = req.params.email;
+    res.send(`Retrieved use with email of ${email}`);
+});
+app.put("/profile/:email", (req, res) => {
+    res.send("User profile updated");
+});
+app.post("/logout", (req, res) => {
+    res.send("User logged out");
+});
+
+// PRODUCTS
+app.get("/products", (req, res) => {
+    const productDetails = req.query;
+    // return the queries for testing
+    res.json(productDetails);
+});
+app.post("/products", (req, res) => {
+    const newProduct = req.body;
+    res.json(newProduct);
+});
+app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Getting product with id ${id}`);
+});
+app.delete("/products/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Deleting product with id ${id}`);
+});
+app.put("/products/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Updating product with id ${id}`);
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
