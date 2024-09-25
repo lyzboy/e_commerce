31919aca824025ffea0c04f8cbaf6e20239756f0
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-09-25 10:20:45 -0600

    Add comments for organization

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "ecommerce",
-    password: "postgres",
-    port: 5432,
+    user: "postgres", //TODO: move to env
+    host: "localhost", //TODO: move to env
+    database: "ecommerce", //TODO: move to env
+    password: "postgres", //TODO: move to env
+    port: 5432, //TODO: move to env
 });
 
 const query = (text, params, callback) => {
