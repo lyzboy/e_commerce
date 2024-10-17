89a800cf22a3b70db0f7f4f5444e4479a7b0cc7f
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-10-17 15:49:23 -0600

    Refactor category integration test file and add update category test

--- backend/tests/integration/category.test.js
+++ backend/tests/integration/category.test.js
@@ -1,3 +1,18 @@
+// use supertest to mock the HTTP requests
 const request = require("supertest");
+// import the app for testing category-routes and category-controller without having to run the server
 const app = require("../../src/app");
+// import the category model so jest can mock the database functionality.
 const categoryModel = require("../../src/models/category");
+
+// mock the category model to simulate access to the database
+jest.mock("../../../models/category-model");
+
+describe("PUT /api/v1/categories/:id - Update Category", () => {
+    // make test use async since category model is async
+    it("should update a category and return the updated category", async () => {
+        // Arrange
+        // Act
+        // Assert
+    });
+});
