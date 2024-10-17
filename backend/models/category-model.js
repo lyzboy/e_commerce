2c1d2552c59418de8f4f56995f1470e18bd97dd0
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-16 20:30:06 -0600

    Refactor category model and routes

--- backend/models/category-model.js
+++ backend/models/category-model.js
@@ -4,43 +4,37 @@ const { query } = require("../config/db");
  * Queries the database to retrieve all categories.
  */
 exports.getCategories = async () => {
-        const results = await query(
-            "SELECT * FROM categories ORDER BY name ASC"
-        );
-        return results.rows;
-
+    const results = await query("SELECT * FROM categories ORDER BY name ASC");
+    return results.rows;
 };
 
 /**
  * Creates a new category with provided parameters.
  * @param {string} name - the name of the category
  * @param {string} description - the description of the object
- * @returns 
+ * @returns
  */
 exports.createCategory = async (name, description) => {
-        const results = await query(
-            "INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *",
-            [name, description]
-        );
-        return results.rows[0];
+    const results = await query(
+        "INSERT INTO categories (name, description) VALUES ($1, $2) RETURNING *",
+        [name, description]
+    );
+    return results.rows[0];
 };
 
 exports.getCategory = async (id) => {
-    const results = await query(
-        "SELECT * FROM categories WHERE id = $1", [id]
-    );
-    results.rows[0]
-}
+    const results = await query("SELECT * FROM categories WHERE id = $1", [id]);
+    results.rows[0];
+};
 
 exports.updateCategory = async (categoryObject) => {
     const results = await query(
-        "UPDATE categories SET name = $1, description = $2 WHERE id = $3", [categoryObject.name, categoryObject.description, categoryObject.id]
+        "UPDATE categories SET name = $1, description = $2 WHERE id = $3",
+        [categoryObject.name, categoryObject.description, categoryObject.id]
     );
-    return results.rows[0]
-}
+    return results.rows[0];
+};
 
-exports.deleteCategory = async(id) => {
-    const results = await query(
-        "DELETE * FROM categories WHERE id = $1", [id]
-    );
+exports.deleteCategory = async (id) => {
+    const results = await query("DELETE * FROM categories WHERE id = $1", [id]);
 };
--- backend/routes/category-routes.js
+++ backend/routes/category-routes.js
@@ -1,54 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const queries = require ('../models/category-model');
-
-
+const queries = require("../models/category-model");
 
 // Categories
 router.get("/", async (req, res) => {
-  try {
-      const results = await queries.getCategories();
-      res.status(200).json(results);
-  } catch (error) {
-      res.status(500).json({ message: "Server error: " + error.message });
-  }
+    try {
+        const results = await queries.getCategories();
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server error: " + error.message });
+    }
 });
 
 router.post("/", async (req, res) => {
-  try {
-      const results = await queries.createCategory(req.body);
-      res.status(200).json(results);
-  } catch (error) {
-      res.status(500).json({ message: "Server Error: " + error.message });
-  }
+    try {
+        const { name, description } = req.body;
+        if (!name || !description) {
+            return res.status(400).json({ message: "Invalid request object." });
+        }
+        const results = await queries.createCategory(name, description);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
 });
 
-router.post("/:id", async(req, res)=>{
-  try {
-    const results = await queries.getCategory(req.params.id);
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ message: "Server Error: "+ error.message});
-  }
+router.post("/:id", async (req, res) => {
+    try {
+        const results = await queries.getCategory(req.params.id);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
 });
 
-router.put("/:id", async(req, res)=>{
-  try {
-    const results = await queries.updateCategory(req.body);
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ message: "Server Error: "+ error.message});
-  }
+router.put("/:id", async (req, res) => {
+    try {
+        const results = await queries.updateCategory(req.body);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
 });
 
-router.delete("/:id", async (req, res)=>{
-  try {
-    const results = await queries.deleteCategory(req.params.id);
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({message: "Server Error: "+error.message});
-  }
+router.delete("/:id", async (req, res) => {
+    try {
+        const results = await queries.deleteCategory(req.params.id);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
 });
 
 module.exports = router;
