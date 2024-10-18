2283f562807160e29d695ed32b1251bcfaff0148
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-17 19:47:35 -0600

    Refactor auth middleware and add token generation unit test

--- backend/tests/unit/auth.test.js
+++ backend/tests/unit/auth.test.js
@@ -1,4 +1,31 @@
 const jwt = require("jsonwebtoken");
-const auth = require("../auth");
+const auth = require("../../middlewares/auth");
 
 jest.mock("jsonwebtoken");
+
+describe("Auth - Unit tests", () => {
+    describe("generateAccessToken", () => {
+        it("should generate a token with the correct payload and expiration", () => {
+            // ARRANGE
+            const mockPayload = {
+                id: "1",
+                username: "user",
+                role: "user",
+            };
+            const mockToken = "mockToken";
+            // mock the behavior of jwt.sign
+            jwt.sign.mockReturnValue(mockToken);
+
+            // ACT
+            const token = auth.generateAccessToken(mockPayload);
+
+            //  ASSERT
+            expect(jwt.sign).toHaveBeenCalledWith(
+                mockPayload,
+                process.env.TOKEN_SECRET,
+                { expiresIn: "1800s" }
+            );
+            expect(token).toBe(mockToken);
+        });
+    });
+});
