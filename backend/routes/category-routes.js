5a46349b88ec39aec0091844a17195ef96b6bb18
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-17 21:01:37 -0600

    Refactor auth middleware to add token authentication to category routes

--- backend/routes/category-routes.js
+++ backend/routes/category-routes.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/category-controller");
+const authMiddleware = require("../middlewares/auth");
 
-router.get("/", categoryController.getCategories);
+router.get(
+    "/",
+    authMiddleware.authenticateToken,
+    categoryController.getCategories
+);
 
-router.post("/", categoryController.createCategory);
+router.post(
+    "/",
+    authMiddleware.authenticateToken,
+    authMiddleware.checkAdminRole,
+    categoryController.createCategory
+);
 
-router.get("/:id", categoryController.getCategory);
+router.get(
+    "/:id",
+    authMiddleware.authenticateToken,
+    categoryController.getCategory
+);
 
-router.put("/:id", categoryController.updateCategory);
+router.put(
+    "/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.checkAdminRole,
+    categoryController.updateCategory
+);
 
-router.delete("/:id", categoryController.deleteCategory);
+router.delete(
+    "/:id",
+    authMiddleware.authenticateToken,
+    authMiddleware.checkAdminRole,
+    categoryController.deleteCategory
+);
 
 module.exports = router;
