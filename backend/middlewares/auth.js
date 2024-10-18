67865f66f023c1c4cca95fd022d81e60c8b4c8fe
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-17 19:36:39 -0600

    Refactor auth middleware and add token generation function

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,11 @@
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 //get config vars
 dotenv.config();
 
 // access config var for the token secret
-process.env.TOKEN_SECRET;
+const tokenSecret = process.env.TOKEN_SECRET;
 
 /*
 app.post('/api/createNewUser', (req, res) => {
@@ -15,9 +16,45 @@ app.post('/api/createNewUser', (req, res) => {
   
     // ...
   });
+
+  cookie storage
+  // get token from fetch request
+    const token = await res.json();
+
+    // set token in cookie
+    document.cookie = `token=${token}`
 */
 
-exports.generateAccessToken = (id) => {
+exports.generateAccessToken = (payload) => {
     // expires in 30 mins
-    return jwt.sign(id, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+    return jwt.sign(payload, tokenSecret, { expiresIn: "1800s" });
+};
+
+exports.authenticateToken = (req, res, next) => {
+    // get the authorization header
+    const authHeader = req.headers["authorization"];
+    //
+    const token = authHeader && authHeader.split(" ")[1];
+
+    // if no token, return 401 status
+    if (!token) return res.status(401).json({ message: "No auth token" });
+
+    jwt.verify(token, tokenSecret, (err, user) => {
+        if (err) {
+            return res
+                .status(403)
+                .json({ message: "Authentication error: " + err.message });
+        }
+        req.user = user;
+
+        next();
+    });
+};
+
+exports.checkAdminRole = (req, res, next) => {
+    if (req.user.rol !== "admin") {
+        return res.status(403).json({ message: "Access denied" });
+    }
+
+    next();
 };
---
+++ backend/tests/unit/auth.test.js
@@ -0,0 +1,4 @@
+const jwt = require("jsonwebtoken");
+const auth = require("../auth");
+
+jest.mock("jsonwebtoken");
