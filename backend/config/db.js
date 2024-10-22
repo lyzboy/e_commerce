6e1ada2c64b9351009cde6a95964419ad51d6438
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-10-22 11:17:11 -0600

    Add jsdoc

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,10 @@ const path = require("path");
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const { Pool } = require("pg");
 
-// Admin pool
+/**
+ * This is the pool to use for Admin users. This pool has full access
+ * to HTTP and database statements.
+ */
 const adminPool = new Pool({
     user: process.env.ADMIN_USER_NAME,
     host: process.env.POOL_HOST_NAME,
@@ -12,7 +15,10 @@ const adminPool = new Pool({
     port: process.env.POOL_PORT,
 });
 
-// Standard user pool
+/**
+ * This is the pool to use for standard users. This pool is limited
+ * to the HTTP and database statements that can be used.
+ */
 const standardPool = new Pool({
     user: process.env.STANDARD_USER_NAME,
     host: process.env.POOL_HOST_NAME,
@@ -21,8 +27,14 @@ const standardPool = new Pool({
     port: process.env.POOL_PORT,
 });
 
-// Function to query with the appropriate pool
-const query = async (text, params, isAdmin = false) => {
+/**
+ * This function is used to query the database.
+ * @param {string} queryText - The query statement
+ * @param {*} params - Any query parameters
+ * @param {*} isAdmin - Determines where to use the adminPool or standardPool
+ * @returns
+ */
+const query = async (queryText, queryParams, isAdmin = false) => {
     try {
         const pool = isAdmin ? adminPool : standardPool;
         const result = await pool.query(text, params);
