57cfa3bd025b52e64f98ac551a78c68cc48d0a33
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-10-22 14:08:57 -0600

    Add product CRUD operations in product-controller.js and product-model.js

---
+++ backend/controllers/product-controller.js
@@ -0,0 +1,5 @@
+exports.getProducts = async () => {};
+exports.createProduct = async () => {};
+exports.getProduct = async () => {};
+exports.updateProduct = async () => {};
+exports.deleteProduct = async () => {};
---
+++ backend/models/product-model.js
@@ -0,0 +1,7 @@
+const { query } = require("../config/db");
+
+exports.getProducts = async () => {};
+exports.createProduct = async () => {};
+exports.getProduct = async () => {};
+exports.updateProduct = async () => {};
+exports.deleteProduct = async () => {};
