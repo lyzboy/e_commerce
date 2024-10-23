e4b297c086e4956d003a90f8d5b2d1b878ff19a4
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-22 21:21:46 -0600

    Refactor product controller to handle filtering by category and price range

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -1,5 +1,22 @@
-exports.getProducts = async () => {};
-exports.createProduct = async () => {};
-exports.getProduct = async () => {};
-exports.updateProduct = async () => {};
-exports.deleteProduct = async () => {};
+exports.getProducts = async (req, res) => {
+    try {
+        const { id, maxPrice, minPrice } = req.body;
+
+        // Create a params object with only defined values
+        const params = {};
+        if (categoryId) params.categoryId = categoryId;
+        if (maxPrice) params.maxPrice = maxPrice;
+        if (minPrice) params.minPrice = minPrice;
+
+        // Pass params object to the model function
+        const results = await productModel.getCategories(params);
+
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
+};
+exports.createProduct = async (req, res) => {};
+exports.getProduct = async (req, res) => {};
+exports.updateProduct = async (req, res) => {};
+exports.deleteProduct = async (req, res) => {};
--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -1,6 +1,16 @@
 const { query } = require("../config/db");
 
-exports.getProducts = async () => {};
+exports.getProducts = async (paramsObject) => {
+    const { categoryId, minPrice, maxPrice } = paramsObject;
+    const queryText = "SELECT * FROM products";
+    const queryParams = [];
+    if (categoryID) {
+        queryParams.push(categoryID);
+        // join table category products
+    }
+    const results = query(queryText, queryParams);
+    return results.rows;
+};
 exports.createProduct = async () => {};
 exports.getProduct = async () => {};
 exports.updateProduct = async () => {};
