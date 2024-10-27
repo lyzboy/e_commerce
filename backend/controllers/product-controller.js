71d16eb7d0dd4da128035cdfe97071fb60cdc05c
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-10-26 23:15:11 -0600

    Refactor product controller and model to add support for creating products

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -18,7 +18,27 @@ exports.getProducts = async (req, res) => {
     }
 };
 exports.createProduct = async (req, res) => {
-    res.status(500).json({ message: "Not implemented" });
+    try {
+        const { barcode, name, description, price, stock_quantity } = req.body;
+
+        if (!name) {
+            return res
+                .status(400)
+                .json({ message: "Bad Request. Missing product name" });
+        }
+
+        const params = {};
+        params.name = name;
+
+        if (barcode) params.barcode = barcode;
+        if (description) params.description = description;
+        if (price) params.price = price;
+        if (stock_quantity) params.stock_quantity = stock_quantity;
+        const results = await productModel.createProduct(params);
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ message: "Error: " + error });
+    }
 };
 exports.getProduct = async (req, res) => {
     res.status(500).json({ message: "Not implemented" });
--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -34,7 +34,50 @@ exports.getProducts = async ({ categoryId, minPrice, maxPrice }) => {
     return results.rows;
 };
 
-exports.createProduct = async () => {};
+exports.createProduct = async (productObject) => {
+    try {
+        const { barcode, name, description, price, stock_quantity, variants } =
+            productObject;
+
+        let queryText = "INSERT INTO products ";
+        let fieldsArray = [];
+        let queryParams = [];
+        let valuesArray = [];
+
+        if (barcode) {
+            fieldsArray.push("barcode");
+            queryParams.push(barcode);
+            valuesArray.push(`$${queryParams.length}`);
+        }
+        if (name) {
+            fieldsArray.push("name");
+            queryParams.push(name);
+            valuesArray.push(`$${queryParams.length}`);
+        }
+        if (description) {
+            fieldsArray.push("description");
+            queryParams.push(description);
+            valuesArray.push(`$${queryParams.length}`);
+        }
+        if (price) {
+            fieldsArray.push("price");
+            queryParams.push(price);
+            valuesArray.push(`$${queryParams.length}`);
+        }
+        if (stock_quantity) {
+            fieldsArray.push("stock_quantity");
+            queryParams.push(stock_quantity);
+            valuesArray.push(`$${queryParams.length}`);
+        }
+        queryText += `(${fieldsArray.join(", ")}) `;
+        queryText += `VALUES (${valuesArray.join(", ")}) `;
+        queryText += `RETURNING *`;
+        const results = await query(queryText, queryParams, true);
+        return results.rows[0];
+    } catch (error) {
+        throw new Error(error);
+    }
+};
 exports.getProduct = async () => {};
 exports.updateProduct = async () => {};
 exports.deleteProduct = async () => {};
