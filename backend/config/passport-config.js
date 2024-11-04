9b1284d42a631c950599bcc423d48effe897a978
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-11-04 15:48:05 -0700

    Add passport configuration for JWT and OAuth strategies

---
+++ backend/config/passport-config.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookTokenStrategy = require("passport-facebook-token");
+const userModel = require("../models/user-model");
+
+// JWT Strat
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.TOKEN_SECRET,
+};
+
+passport.use(
+  JwtStrategy(opts, async (jwt_payload, done) => {
+    // find the user based on the id in the JWT payload
+    userModel.findById(jwt_payload.id, (err, user) => {
+      if (err) return done(err, false);
+      if (user) return done(null, user);
+      else return done(null, false);
+    });
+  })
+);
