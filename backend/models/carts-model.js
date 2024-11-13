970f56f23fe87281122a504101122fc42b43880c
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-11-13 13:40:11 -0700

    #12 refactor carts-model to inherit from new base_resources-model

--- backend/models/carts-model.js
+++ backend/models/carts-model.js
@@ -1,17 +1,18 @@
 const { query } = require("../config/db");
+const BaseModel = require("./base_resource-model");
 
-/**
- * Gets the cart base on id. Used within the ownership authorization middleware
- * @param {int} id
- * @returns
- */
-exports.getResourceById = async (id) => {
+const CartsModel = Object.create(BaseModel);
+CartsModel.tableName = "carts";
+
+CartsModel.getCartByEmail = async (email) => {
   try {
-    const queryText = `SELECT * FROM carts WHERE id = $1`;
-    const queryParams = [id];
+    const queryText = `SELECT * FROM carts WHERE account_email = $1`;
+    const queryParams = [email];
     const result = await query(queryText, queryParams);
     return result.rows[0];
   } catch (error) {
     throw new Error(error);
   }
 };
+
+module.exports = CartsModel;
