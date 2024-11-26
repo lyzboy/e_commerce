98fc7d9785800ad828f1f282003503ad18bd281b
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-11-25 22:06:27 -0700

    #15 test: add testing connection for test database and modify discount integration test file.

--- backend/tests/integration/discounts.test.js
+++ backend/tests/integration/discounts.test.js
@@ -3,83 +3,16 @@ const express = require("express");
 const discountRoutes = require("../../routes/discounts-routes");
 const discountModel = require("../../models/discounts-model");
 
-jest.mock("../../models/discounts-model", () => ({
-  getDiscountedProducts: jest.fn((limit, page, category) => {
-    const mockData = [
-      { id: 1, name: "Product 1", effective_price: 10 },
-      { id: 2, name: "Product 2", effective_price: 20 },
-    ];
-    return Promise.resolve(mockData);
-  }),
-  getDiscountByCode: jest.fn((code) => {
-    if (code == "10OFF") {
-      const mockData = [
-        { id: 1, code: "10OFF", percent_off: 10, expire_date: "2022-12-31" },
-      ];
-      return Promise.resolve(mockData);
-    } else {
-      return Promise.resolve(null);
-    }
-  }),
-  addDiscountToProduct: jest.fn((productId, discountId, productVariantId) => {
-    return Promise.resolve({
-      id: 1,
-      productId: productId,
-      discountId: discountId,
-      productVariantId: productVariantId ? productVariantId : null,
-    });
-  }),
-  removeDiscountFromProduct: jest.fn(
-    (productDiscountId, productId, discountId, productVariantId) => {
-      if (productDiscountId === 1) {
-        return Promise.resolve(1);
-      } else {
-        return Promise.resolve(0);
-      }
-    }
-  ),
-  deleteDiscount: jest.fn((id) => {
-    if (id === 1) {
-      return Promise.resolve(1);
-    } else {
-      return Promise.resolve(0);
-    }
-  }),
-
-  getDiscount: jest.fn((id) => {
-    if (id === 1) {
-      return Promise.resolve({
-        id: 1,
-        code: "10OFF",
-        percent_off: 10,
-        expire_date: "2022-12-31",
-      });
-    } else {
-      return Promise.resolve(null);
-    }
-  }),
-
-  createDiscount: jest.fn(
-    (code, percentOff, amountOff, expireDate, quantity) => {
-      return Promise.resolve({
-        id: 1,
-        code: code,
-        percent_off: percentOff,
-        amount_off: amountOff,
-        expire_date: expireDate,
-        quantity: quantity,
-      });
-    }
-  ),
-
-  getDiscounts: jest.fn((limit, page, categoryId) => {
-    const mockData = [
-      { id: 1, code: "10OFF", percent_off: 10, expire_date: "2022-12-31" },
-      { id: 2, code: "20OFF", percent_off: 20, expire_date: "2022-12-31" },
-    ];
-    return Promise.resolve(mockData);
-  }),
-}));
+// jest.mock("../../models/discounts-model", () => ({
+//   getDiscountedProducts,
+//   getDiscountByCode,
+//   addDiscountToProduct,
+//   removeDiscountFromProduct,
+//   deleteDiscount,
+//   getDiscount,
+//   createDiscount,
+//   getDiscounts,
+// }));
 
 const app = express();
 
@@ -95,21 +28,21 @@ describe("Discounts Endpoints Integration Tests", () => {
   describe("GET /discounts", () => {
     it("should get all discounts", async () => {
       // arrange
-      const mockData = [
-        { id: 1, code: "10OFF", percent_off: 10, expire_date: "2022-12-31" },
-        { id: 2, code: "20OFF", percent_off: 20, expire_date: "2022-12-31" },
-      ];
-      discountModel.getDiscounts = jest
-        .fn()
-        .mockReturnValue(Promise.resolve(mockData));
 
       // act
       const response = await request(app).get("/discounts");
 
       //assert
       expect(response.status).toBe(200);
-      expect(response.body).toEqual(mockData);
-      expect(discountModel.getDiscounts).toHaveBeenCalled();
+      expect(response.body).toBeDefined();
+      expect(response.body.length).toBeGreaterThan(0); // Check if it's not empty
+
+      // Check the properties of the first discount object (example)
+      const firstDiscount = response.body[0];
+      expect(firstDiscount).toHaveProperty("id");
+      expect(firstDiscount).toHaveProperty("code");
+      expect(firstDiscount).toHaveProperty("percent_off");
+      expect(firstDiscount).toHaveProperty("expire_date");
     });
   });
 });
